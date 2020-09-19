d154aec55d1d44ae680634af0d018f27da2f7a81
Marcella Luz Ash <marcella@ash-photos.com>
2020-09-18 19:04:36 -0700

    wrote out appjs

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import 'react-table-v6/react-table.css'
+import ReactTable from 'react-table-v6'
+
+const App = () => {
+
+  const [employeeState, setEmployeeState] = useState({
+    employees: [],
+    columns: [
+      {
+        Header: 'Name',
+        accessor: 'name'
+      },
+      {
+        Header: 'Phone',
+        accessor: 'phone'
+      },
+      {
+        Header: 'Email',
+        accessor: 'email'
+      }
+    ]
+  })
+
+  useEffect(() => {
+    axios.get('https://randomuser.me/api?results=20')
+      .then(({ data }) => {
+        console.log(data.results)
+
+        let employees = data.results.map(employee => ({
+          name: employee.name.first + '' + employee.name.last,
+          email: employee.email,
+          phone: employee.phone
+        }))
+
+        setEmployeeState({ ...employeeState, employees})
+
+      })
+      .catch(err => console.log(err))
+  }, )
+
+  return (
+    <ReactTable
+      data = {employeeState.employees}
+      columns = {employeeState.columns}
+    />
+  )
+}
+
+export default App
